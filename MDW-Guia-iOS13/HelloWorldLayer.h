d392ac3c02fbfb6f3e8d7590538d7342c6c9aa99
jCalaU <javier@arkelao.co>
2011-10-24 16:37:04 -0500

    Initial commit

---
+++ MDW-Guia-iOS13/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  MDW-Guia-iOS13
+//
+//  Created by Javier Cala Uribe on 24/10/11.
+//  Copyright * 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
